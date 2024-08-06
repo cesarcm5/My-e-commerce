a536f4a26a389351eaf45d2cbeec49a169727276
Cesar Contreras <cesar.alberto2003@outlook.com>
2024-08-06 17:50:31 -0600

    First Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+ 
+
+import "./index.css"
+import greeting from './numberOfproducts'
+
+
+ReactDOM.createRoot(document.getElementById('test')).render(<greeting/>)
+
---
+++ numberOfproducts.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const greeting = () => {
+    return (
+        <p>Hola! estoy testeando JavaScript</p>
+    )
+}
+export default greeting;
